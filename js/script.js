2b5fc77e25e3d77b7000b2aa2f508beefee22923
taejeong123 <auction4289@gmail.com>
2020-08-05 15:35:08 +0900

    20200805_2

--- js/script.js
+++ js/script.js
@@ -23,16 +23,3 @@ function moveSlide(current) {
     setTimeout(() => { isMoving = false; }, 600);
 }
 // 메인 슬라이더
-
-// 모바일 메뉴 이벤트
-$('.nav-icon').on('click', function() {
-    $('.mobile-nav').stop().slideDown();
-    $(this).hide();
-    $('.nav-icon-close').show();
-});
-$('.nav-icon-close').on('click', function() {
-    $('.mobile-nav').stop().slideUp();
-    $(this).hide();
-    $('.nav-icon').show();
-});
-// 모바일 메뉴 이벤트
